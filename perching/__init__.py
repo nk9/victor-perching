55ce778cc69a486ba767da0741bf278b81e74ee1
Nick Kocharhook <nick@kocharhook.com>
2024-07-21 14:17:24 +0100

    Graph 1: observation count by species and perch species

--- perching/__init__.py
+++ perching/__init__.py
@@ -1,35 +1,64 @@
 import pandas as pd
-import plotly.graph_objects as go
-from dash import Dash, Input, Output, dcc, html
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
 
-app = Dash(__name__)
+# Initialize the Dash app
+app = dash.Dash(__name__)
 
-
+# Get unique values for the dropdown options
 df = pd.read_csv("data/victor_observations.csv")
-print(df.head())
+df_filtered = df.dropna(subset=["field:perching on"])
+df_filtered["perching_genus"] = df_filtered["field:perching on"].apply(
+    lambda x: x.split()[0]
+)
+unique_perching_on = sorted(df_filtered["perching_genus"].unique())
 
+df_grouped = (
+    df_filtered.groupby(["perching_genus", "taxon_family_name"])
+    .size()
+    .reset_index(name="count")
+)
+
+print(df_grouped.head())
+
+# Layout of the app
 app.layout = html.Div(
     [
-        html.H4("Interactive color selection with simple Dash example"),
-        html.P("Select color:"),
         dcc.Dropdown(
-            id="dropdown",
-            options=["Gold", "MediumTurquoise", "LightGreen"],
-            value="Gold",
-            clearable=False,
+            id="perching_on_dropdown",
+            options=[{"label": perch, "value": perch} for perch in unique_perching_on],
+            value=unique_perching_on[0],  # Set a default value
         ),
-        dcc.Graph(id="graph"),
+        dcc.Graph(id="family_count_graph"),
     ]
 )
 
 
-@app.callback(Output("graph", "figure"), Input("dropdown", "value"))
-def display_color(color):
+# Callback to update the graph based on selected dropdown value
+@app.callback(
+    Output("family_count_graph", "figure"), Input("perching_on_dropdown", "value")
+)
+def update_graph(selected_perching_on):
+    filtered_df = df_grouped[df_grouped["perching_genus"] == selected_perching_on]
+
     fig = go.Figure(
-        data=go.Bar(
-            y=[2, 3, 1], marker_color=color  # replace with your own data source
-        )
+        data=[
+            go.Bar(
+                x=filtered_df["taxon_family_name"],
+                y=filtered_df["count"],
+                marker=dict(color="blue"),
+            )
+        ]
     )
+
+    fig.update_layout(
+        title=f"Counts of Families Perching on {selected_perching_on}",
+        xaxis_title="Family",
+        yaxis_title="Count",
+    )
+
     return fig
 
 
